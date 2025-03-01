f0827a521c1afb05c10af9d20a18d2e6f26bed8e
nbaker47 <nathan.baker.2647@gmail.com>
2025-03-02 00:57:26 +0800

    fix: 2

---
+++ dt-bindings/zmk/pointing.h
@@ -0,0 +1,9 @@
+#ifndef ZMK_POINTING_H
+#define ZMK_POINTING_H
+
+#define MSC_SCROLL_UP 0x01
+#define MSC_SCROLL_DOWN 0x02
+#define MSC_SCROLL_LEFT 0x03
+#define MSC_SCROLL_RIGHT 0x04
+
+#endif // ZMK_POINTING_H
